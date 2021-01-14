08d2c5db4aa110f60daf65014b21966621805488
Nils Alexandre Bergsten <nilsalexberg@gmail.com>
2021-01-14 10:50:35 -0300

    Nav courses dropdown

--- frontend/js/dropdown-hover.js
+++ frontend/js/dropdown-hover.js
@@ -14,16 +14,3 @@ $(document).ready(function(){
     .on('mouseenter mouseleave','.dropdown',toggleDropdown)
     .on('click', '.dropdown-menu a', toggleDropdown);
 })
-
-var $navBar = $('nav.navbar')
-
-window.onscroll = checkScrollPosition
-window.onload = checkScrollPosition
-
-function checkScrollPosition() {
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-    $navBar.addClass('with-bg')
-  } else {
-    $navBar.removeClass('with-bg')
-  }
-}
